88780f0d7c8559117e53bb0299b2cbfdd38bbed9
LegendAsGamer3248 <finnhendricks05.privat@gmail.com>
2022-07-13 21:46:25 +0200

    Switching to /-commands

--- index.js
+++ index.js
@@ -4,18 +4,40 @@ dotenv.config();
 
 const client = new DiscordJS.Client({
     intents: [
-        Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES
     ]
 })
 
 client.on('ready', () => {
     console.log('Huel is ready.')
+    const guildId = '996435642273779743'
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'online',
+        description: 'Will tell you wether the bot is currently suffering from an outage or not.'
+    })
 })
 
-client.on('messageCreate', (message) => {
-    if (message.content == '/online' && message.channelId == '996747038433738762') {
-        message.react('👍')
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return
+    }
+
+    const { commandName, options } = interaction
+
+    if (commandName === 'ping') {
+        interaction.reply({
+            content: 'Yes, I am online!',
+            ephemeral: true
+        })
     }
 })
 
