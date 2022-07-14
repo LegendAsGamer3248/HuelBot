0ab544e9d10b418b1d3bdfc1dbd22bcdd0af4505
LegendAsGamer3248 <finnhendricks05.privat@gmail.com>
2022-07-14 17:37:07 +0200

    Fixed latest release

--- commands/online.ts
+++ commands/online.ts
@@ -1,13 +1,11 @@
-import { ICommand } from "wokcommands"
-
 module.exports = {
     category: 'Testing',
     description: 'Does not respond if Bot is down',
-
+    
     salsh: true,
     testOnly: true,
-
+    
     callback: ({}) => {
         return 'I am here!'
     }
-} as ICommand
+}
