3f129a0e49fccd80994b94f41f0486e3723777d9
LegendAsGamer3248 <finnhendricks05.privat@gmail.com>
2022-07-14 12:49:11 +0200

    ReferenceError: __dirname isn't defined. Fixes #1

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import DiscordJS, { Intents } from 'discord.js' // DiscordJS
 import WOKCommands from 'wokcommands' // Command Handler
-import path from 'path' //
+import path from 'path' // Gets path for Command Handler
+import { fileURLToPath } from 'url' // Required to fix __dirname not defined Error.
 import dotenv from 'dotenv'  // => Only used for testing on your OWN PC, not really needed.
 // dotenv.config();
+
+// const __filename = fileURLToPath(import.meta.url); // Used for __dirname
+// const __dirname = path.dirname(__filename); // Used to fix __dirname not defined Error.
 
 const client = new DiscordJS.Client({
     intents: [
@@ -14,7 +18,7 @@ client.on('ready', () => {
     console.log('Huel is ready.')
 
     new WOKCommands(client, {
-        commandDir: path.join(__dirname, 'commands'),
+        commandsDir: path.join(__dirname, 'commands'),
         testServers: ['996435642273779743']
     })
 })
