6b223682ead3b2eb45a4703776ecceb210590a62
LegendAsGamer3248 <finnhendricks05.privat@gmail.com>
2022-07-14 17:19:37 +0200

    pog

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 import dotenv from 'dotenv'  // => Only used for testing on your OWN PC, not really needed.
-dotenv.config();
+// dotenv.config();
 
 const client = new DiscordJS.Client({
     intents: [
